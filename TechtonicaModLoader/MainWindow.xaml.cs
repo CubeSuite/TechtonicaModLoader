ee27024d4f88afc9fea91d199e40c9348e4f0a21
CubeSuite <cubesuite@outlook.com>
2024-05-11 12:45:36 +0100

    Game is now given focus after launching

--- TechtonicaModLoader/MainWindow.xaml.cs
+++ TechtonicaModLoader/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ using System.Windows.Shapes;
 using System.Windows.Threading;
 using TechtonicaModLoader.Modes;
 using TechtonicaModLoader.MyClasses;
+using TechtonicaModLoader.MyClasses.Globals;
 using TechtonicaModLoader.MyPanels;
 using TechtonicaModLoader.MyWindows;
 
@@ -98,7 +99,6 @@ namespace TechtonicaModLoader
         }
 
         private void OnProcessCheckTimerTick(object sender, EventArgs e) {
-            return; // ToDo: Remove this
             Process[] techtonicaProcess = Process.GetProcessesByName("Techtonica");
             if (techtonicaProcess.Length != 0) {
                 launchGameButton.IsEnabled = false;
@@ -106,12 +106,17 @@ namespace TechtonicaModLoader
                 loadingPanel.SetInfo("Game Is Running");
                 mainGrid.Visibility = Visibility.Hidden;
                 loadingPanel.Visibility = Visibility.Visible;
+
+                if (!WindowSwitcher.gaveFocusSinceLaunch) {
+                    WindowSwitcher.FocusGame();
+                }
             }
             else {
                 launchGameButton.IsEnabled = true;
                 launchGameButton.ButtonText = "Launch Game";
                 mainGrid.Visibility = Visibility.Visible;
                 loadingPanel.Visibility = Visibility.Hidden;
+                WindowSwitcher.gaveFocusSinceLaunch = false;
             }
         }
 
